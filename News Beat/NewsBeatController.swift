dac8c07a84096a0be6caba3d156e9591583dda73
Batuhan <batu.omer@icloud.com>
2023-03-08 23:15:13 +0300

    Initial Commit

---
+++ News Beat/NewsBeatController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Beat
+//
+//  Created by Batuhan Yılmaz on 8.03.2023.
+//
+
+import UIKit
+
+class NewsBeatController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
